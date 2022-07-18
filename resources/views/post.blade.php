487168fae96d7f4e072dbe5b03b9d4e367383430
Giovanni Kurniawan <giovanni.kurniawan@student.umn.ac.id>
2022-07-18 12:21:58 +0700

    fillable

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,14 +1,10 @@
 @extends('layouts.main')
 
 @section('container')
-    <article>
-        <h2>{{ $post['title'] }}</h2>
-        <h3>{{ $post['author'] }}</h3>
-        <p>{{ $post['body'] }}</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime deserunt temporibus voluptatem, enim ipsum
-            distinctio saepe alias numquam adipisci voluptatibus id provident corrupti error itaque delectus explicabo
-            voluptas aspernatur aperiam?</p>
-    </article>
+    <h1 class="mb-5">{{ $post->title }}</h1>
+
+
+    {!! $post->body !!}
 
     <a href="/posts">Back to Post</a>
 @endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.main')
 @section('container')
+    <h1 class="mb-5">Halaman Blog Post</h1>
     @foreach ($posts as $post)
-        <article class="mb-5">
+        <article>
             <h2>
-                <a href="/posts/{{ $post['slug'] }}"> {{ $post['title'] }}</a>
+                <a href="/posts/{{ $post->id }}"> {{ $post->title }}</a>
             </h2>
-            <h5>{{ $post['author'] }}</h5>
-            <p>{{ $post['body'] }}</p>
+            <p>{{ $post->excerpt }}</p>
         </article>
     @endforeach
 @endsection
