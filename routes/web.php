308865cfe776b95788ee48620b261ed5503138f6
Giovanni Kurniawan <giovanni.kurniawan@student.umn.ac.id>
2022-07-18 19:02:36 +0700

    Route Model Binding

--- routes/web.php
+++ routes/web.php
@@ -37,4 +37,4 @@ Route::get('/posts', [PostController::class, 'index']);
 
 
 //halaman single post
-Route::get('/posts/{slug}', [PostController::class, 'show']);
+Route::get('/posts/{post}', [PostController::class, 'show']);
